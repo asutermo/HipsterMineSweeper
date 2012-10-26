3db2d9b7abe98cdb84ea9db81e96bb723e16bf69
asutermo <asutermo@mymail.mines.edu>
2012-10-25 18:27:34 -0600

    added game vars and some button vars

--- scripts/game.js
+++ scripts/game.js
@@ -4,13 +4,43 @@ function gameSetup() {
     var GAME_HEIGHT = 700;
     var GAME_FPS = 60;
 
-
     var GAME_STATE = {
-        WON = 'won';
-        PLAY = 'play';
-        LOST = 'lost';
-        PAUSE = 'pause';   
-    }
+        WON = 'won',
+        PLAY = 'play',
+        LOST = 'lost',
+        PAUSE = 'pause'   
+    };
+
+    var map = {
+        width: GAME_WIDTH,
+        height: GAME_HEIGHT;   
+    };
+
+    //set up mouse
+    var mousePos = {
+        x: 0,
+        y: 0,
+        isDown: false,
+    };
+
+    //set up img vars
+    
+
+    //set up game vars
+    var gameInPlay = false;
+    var gamePaused = false;
+    var finished = false;
+    var towerArray = [];
+    var enemyArray = [];
+    var player;
+    var level;
+    var time;
+
+    //button vars
+    var startBut;
+    var pauseBut;
+
+
 }
 
 function gameInit() {
