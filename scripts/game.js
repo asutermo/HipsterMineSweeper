6d8fd93075e853c1e3be76e196fb5fba82992b8a
asutermo <asutermo@mymail.mines.edu>
2012-10-25 18:20:11 -0600

    added game defaults and states

--- scripts/game.js
+++ scripts/game.js
@@ -1,9 +1,20 @@
 function gameSetup() {
+    //screen settings
+    var GAME_WIDTH = 700;
+    var GAME_HEIGHT = 700;
+    var GAME_FPS = 60;
 
+
+    var GAME_STATE = {
+        WON = 'won';
+        PLAY = 'play';
+        LOST = 'lost';
+        PAUSE = 'pause';   
+    }
 }
 
 function gameInit() {
-    
+
 }
 
 function exit() {
@@ -19,5 +30,13 @@ function gameOver() {
 }
 
 function pause() {
+
+}
+
+function update() {
+
+}
+
+function draw() {
 
 }
