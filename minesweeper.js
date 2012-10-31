12ad83ec30da9fb831cfebda2e0f1952fd4d7091
asutermo <asutermo@mymail.mines.edu>
2012-10-30 22:00:47 -0600

    added jquery

--- minesweeper.js
+++ minesweeper.js
@@ -1,36 +1,45 @@
-	var gameBoard = {
-			rows: 10,
-			cols: 10,
-			width: 50,
-			height: 50,
-			fps: 30
-	};
+	
 
-	var canvasDrawContext;
-	var mouseXPos;
-	var mouseYPos;
-	var gridUnit;
+var gameBoard = {
+		rows: 10,
+		cols: 10,
+		width: 50,
+		height: 50,
+		fps: 30
+};
 
-	function init() {
-		var canvas = document.getElementById("game_canvas");
-		canvasDrawContext = canvas.getContext("2d"); 
-		gridUnit = new Image();
-		gridUnit.src = "images/starbucks_cup.png";
-		drawGrid();
-	}
+var canvasDrawContext;
+var mouseXPos;
+var mouseYPos;
+var gridUnit;
 
-	function drawGrid() {
-		canvasDrawContext.clearRect(0, 0, 400, 400);
-		for (var current_row = 0; current_row < gameBoard.rows; current_row++) {
-			for (var current_col = 0; current_col < gameBoard.cols; current_col++) {
-				var y_position = current_row * gameBoard.height;
-				var x_position = current_col * gameBoard.width;
-				canvasDrawContext.drawImage(gridUnit, x_position, y_position);
-			}
+function init() {
+	var canvas = document.getElementById("game_canvas");
+	canvasDrawContext = canvas.getContext("2d"); 
+
+	document.addEventListener("mouseclick", click, false);
+
+	gridUnit = new Image();
+	gridUnit.src = "images/starbucks_cup.png";
+
+
+	drawGrid();
+}
+
+function drawGrid() {
+	canvasDrawContext.clearRect(0, 0, 400, 400);
+	for (var current_row = 0; current_row < gameBoard.rows; current_row++) {
+		for (var current_col = 0; current_col < gameBoard.cols; current_col++) {
+			var y_position = current_row * gameBoard.height;
+			var x_position = current_col * gameBoard.width;
+			canvasDrawContext.drawImage(gridUnit, x_position, y_position);
 		}
-	}	
-
-	document.addEventListener("DOMContentLoaded", init, false);
-
+	}
+}	
+function click(e) {
+	mouseXPos = e.pageX;
+	mouseYPos = e.pageY;
+	console.log(mouseXPos + ", " + mouseYPos);
+}
 
 
